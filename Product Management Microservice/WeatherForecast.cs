da44130cb8855cf3fdb98af0c95ab992b3d62d26
deepika977 <57782112+deepika977@users.noreply.github.com>
2022-06-30 21:47:15 +0530

    finished add product feature

--- Product Management Microservice/WeatherForecast.cs
+++ Product Management Microservice/WeatherForecast.cs
@@ -2,6 +2,7 @@ namespace Product_Management_Microservice
 {
     public class WeatherForecast
     {
+        public int id { get; set; }
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
